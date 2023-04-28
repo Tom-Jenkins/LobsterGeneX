ee647018f6b072659da6bd6d025ef6d241bd767b
Tom Jenkins <tom.l.jenkins@outlook.com>
2023-04-28 10:53:32 +0100

    Remove redundant files

--- src/js/parse.js
+++
@@ -1,8 +0,0 @@
-// Import text file
-Papa.parse("./src/data/count_data_filtered_with_gene_names.csv", {
-    header: true,
-    download: true,
-	complete: function(results) {
-		console.log("Finished:", results);
-	}
-});
