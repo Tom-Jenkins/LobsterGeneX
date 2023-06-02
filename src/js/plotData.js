1c5b2a632a78a333af29cb0a49de47d541a897fb
tomjenkins <Tom.Jenkins@apha.gov.uk>
2023-06-02 16:36:48 +0100

    Add colour per boxplot

--- src/js/plotData.js
+++ src/js/plotData.js
@@ -7,6 +7,9 @@
 // Import JS from ECharts
 import * as echarts from "echarts";
 
+// Order of tissues
+const tissues = ["Eye", "Gill", "Nerve", "Muscle", "Heart", "Hepatopancreas", "Gut", "Ovary", "Testes", "Juvenile"];
+
 // Initiate echarts variable
 export let echartsPlot;
 
@@ -31,6 +34,8 @@ export function renderPlot (data) {
         // Specify the configuration items and data for the chart
         const option = {
 
+            // TODO: Create a raw points data set using help from echarts examples
+            
             // DATA
             dataset: [
                 {
@@ -39,29 +44,39 @@ export function renderPlot (data) {
                      // E.g. for one tissue: [eye1, eye2, eye3, eye4]
                     [selectedGeneData.eye_1, selectedGeneData.eye_2, selectedGeneData.eye_3, selectedGeneData.eye_4],
                     [selectedGeneData.gill_1, selectedGeneData.gill_2, selectedGeneData.gill_3, selectedGeneData.gill_4],
-                    [selectedGeneData.gonad_1, selectedGeneData.gonad_2, selectedGeneData.gonad_3, selectedGeneData.gonad_4],
-                    [selectedGeneData.gut_1, selectedGeneData.gut_2, selectedGeneData.gut_3, selectedGeneData.gut_4],
+                    [selectedGeneData.nerve_1, selectedGeneData.nerve_2, selectedGeneData.nerve_3, selectedGeneData.nerve_4],
+                    [selectedGeneData.muscle_1, selectedGeneData.muscle_2, selectedGeneData.muscle_3, selectedGeneData.muscle_4],
                     [selectedGeneData.heart_1, selectedGeneData.heart_2, selectedGeneData.heart_3, selectedGeneData.heart_4],
                     [selectedGeneData.hepato_1, selectedGeneData.hepato_2, selectedGeneData.hepato_3, selectedGeneData.hepato_4],
-                    [selectedGeneData.juv_1, selectedGeneData.juv_2, selectedGeneData.juv_3, selectedGeneData.juv_4],
-                    [selectedGeneData.muscle_1, selectedGeneData.muscle_2, selectedGeneData.muscle_3, selectedGeneData.muscle_4],
-                    [selectedGeneData.nerve_1, selectedGeneData.nerve_2, selectedGeneData.nerve_3, selectedGeneData.nerve_4],
+                    [selectedGeneData.gut_1, selectedGeneData.gut_2, selectedGeneData.gut_3, selectedGeneData.gut_4],
                     [selectedGeneData.ovary_1, selectedGeneData.ovary_2, selectedGeneData.ovary_3, selectedGeneData.ovary_4],
+                    [selectedGeneData.gonad_1, selectedGeneData.gonad_2, selectedGeneData.gonad_3, selectedGeneData.gonad_4],
+                    [selectedGeneData.juv_1, selectedGeneData.juv_2, selectedGeneData.juv_3, selectedGeneData.juv_4],
                 ],
-            }, {
-                transform: {
-                    type: "boxplot",
+                }, {
+                    transform: {
+                        type: "boxplot",
+                        config: { 
+                            itemNameFormatter: function (params) {
+                                return tissues[params.value];
+                            }
+                        }
+                    }
                 }
-            }],
+            ],
 
             // SERIES
             series: [
                 {
-                name: "boxplot",
+                name: "Boxplot",
                 type: "boxplot",
                 datasetIndex: 1,
+                colorBy: "data",
                 }
             ],
+
+            // COLOURS
+            color: ["#F5EC88","#E4CC75","#F6C578","#DDA643","#C98B4D","#C1D49A","#7F8C63","#2B5B77","#96BED0","#5A5A56"],
 
             // TITLE
             title: {
@@ -73,22 +88,24 @@ export function renderPlot (data) {
 
             // LEGEND
             legend: {
-                // data : ["Eye", "Gill", "Gonad", "Gut", "Heart", "Juvenile", "Muscle", "Nerve", "Ovary"],
+                show: true,
                 left: "right",
+                formatter: "Toggle {name}"
             },
 
             // XAXIS
             xAxis: {
                 type: "category",
-                data : ["Eye", "Gill", "Gonad", "Gut", "Heart", "Juvenile", "Muscle", "Nerve", "Ovary"],
                 boundaryGap: true,
-                namegap: 30,
                 splitArea: {
                     show: false
                 },
                 splitLine: {
                     show: false
                 },
+                axisLabel: {
+                    rotate: 90
+                }
             },
 
             // YAXIS
