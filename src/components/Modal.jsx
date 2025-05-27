ae9cb9ff93ed2a7be24552f3353bb83620b9e7e1
Jenkins <t.l.jenkins@exeter.ac.uk>
2025-05-27 14:53:01 +0100

    add a modal component when info icon clicked

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,34 @@
-// TODO
+import { useEffect, useRef } from "react";
 
-// change tooltip title to bold
+export default function Modal({ isOpen, renderCloseBtn = true, onClose, children }) {
+    const modalRef = useRef(null);
 
-// Change tooltip classes to match react app (not bootstrap)
+    useEffect(() => {
+        // Get reference to the modal DOM element
+        const modalElement = modalRef.current;
+        if (!modalElement) return; // guard clause
+
+        // Open modal when 'isOpen' changes to true
+        isOpen ? modalElement.showModal() : modalElement.close();
+
+    }, [isOpen]);
+
+    function handleCloseModal() {
+        onClose && onClose();
+    }
+
+    function handleEscKeydown(e) {
+        if (e.key === "Escape") handleCloseModal();
+    }
+
+    return (
+        <dialog ref={modalRef} className="modal" onKeyDown={handleEscKeydown}>
+            {children}
+            {renderCloseBtn &&
+                <button onClick={handleCloseModal} className="modal-close-btn">
+                    Close
+                </button>
+            }
+        </dialog>
+    );
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,8 @@ import book from "../assets/book.svg";
 // import github from "../assets/github.svg";
 import logo from "/logo.svg"
 import infoCircle from "../assets/info-circle.svg";
+import { useState } from "react";
+import Modal from "./Modal";
 
 const navbar = {
     display: "flex",
@@ -16,25 +18,54 @@ const navbar = {
 }
 
 export default function NavBar() {
+    const [modalOpen, setModalOpen] = useState(false);
 
-  return (
-    <nav style={navbar}>
-        <div className="navbar__title">
-            <img className="navbar__title--icon" src={icon} alt="lobster icon" />
-            <a href="https://www.lobstergenex.com/">LobsterGeneX</a>
-            </div>
+    function handleModalOpen() {
+        setModalOpen(true);
+    }
 
-            <div className="navbar__icons--container">
-            <a href="#">
-                <img className="navbar__icons" src={infoCircle} alt="information icon" />
-            </a>
-            <a href="https://metazoa.ensembl.org/Homarus_gammarus_gca958450375v1/Info/Index">
-                <img className="navbar__icons" src={book} alt="book icon" />
-            </a>
-            <a href="https://tomjenkins.co.uk">
-                <img className="navbar__icons" src={logo} alt="logo icon" />
-            </a>
-        </div>
-    </nav>
-  );
+    function handleModalClose() {
+        setModalOpen(false);
+    }
+
+    return (
+        <>
+            <nav style={navbar}>
+                <div className="navbar__title">
+                    <img className="navbar__title--icon" src={icon} alt="lobster icon" />
+                    <a href="https://www.lobstergenex.com/">LobsterGeneX</a>
+                    </div>
+
+                    <div className="navbar__icons--container">
+                    <a onClick={handleModalOpen} style={{cursor: "pointer"}}>
+                        <img className="navbar__icons" src={infoCircle} alt="information icon" />
+                    </a>
+                    <a href="https://metazoa.ensembl.org/Homarus_gammarus_gca958450375v1/Info/Index">
+                        <img className="navbar__icons" src={book} alt="book icon" />
+                    </a>
+                    <a href="https://tomjenkins.co.uk">
+                        <img className="navbar__icons" src={logo} alt="logo icon" />
+                    </a>
+                </div>
+            </nav>
+
+            <Modal isOpen={modalOpen} renderCloseBtn={true} onClose={handleModalClose} >
+                <h4>About LobsterGeneX</h4>
+                {/* <p>
+                    LobsterGeneX is a web application designed to visualise gene expression results from ten different tissue types from the European lobster (<em>Homarus gammarus</em>).
+                </p>
+                <p>
+                    Tissue types analysed:
+                    <ul>
+                        <li>Eye, Gill, Nerve, Muscle, Heart</li>
+                        <li>Hepatopancreas, Gut</li>
+                        <li>Ovary, Testes</li>
+                        <li>Juvenile</li>
+                    </ul>  
+                </p>
+                <p class="fw-bold">Citation</p>
+                <p>XXX</p> */}
+            </Modal>
+        </>
+    );
 }
